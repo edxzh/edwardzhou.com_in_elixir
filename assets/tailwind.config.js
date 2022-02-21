4df4d424fd231edac4481382bb896f3b019c5fa6
Edward Zhou <Edward_mjz@hotmail.com>
2022-02-21 20:48:34 +1100

    Add favicon

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-once': 'spin 1s linear'
+      }
+    },
   },
   variants: [],
   plugins: [require("@tailwindcss/forms")],
